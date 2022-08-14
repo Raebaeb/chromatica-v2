fe472cc80f57a95db2260e63d68aaa0203863c26
Rachel P <rlb2rlp@gmail.com>
2022-08-14 13:51:49 -0700

    install express

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const app = require("express")();
+const PORT = 8080;
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT} 🚀`));
+
+app.get("/", (req, res) => {
+  res.status(200).send("You are on the homepage");
+});
